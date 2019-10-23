1b72132f35e38a63f744f0e07fc2ba0042e2bce1
eric-tong <erictong.designs@gmail.com>
2019-10-23 16:42:53 +0100

    ENR-4 Put new location onto server from device

--- app/src/main/java/com/example/enroute/beacon/MainActivity.kt
+++ app/src/main/java/com/example/enroute/beacon/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.enroute.beacon
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.enroute.beacon.api.PlatformApi
+import com.example.enroute.beacon.helper.enqueue
+import com.example.enroute.beacon.helper.Result
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val service = Retrofit.Builder()
+            .baseUrl("https://enroute-platform.herokuapp.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create<PlatformApi>(PlatformApi::class.java)
+
+        val lat = 444F
+        val long = 444F
+
+        service.putLocation(lat, long).enqueue { result ->
+            when (result) {
+                is Result.Success -> {
+                    val locations = result.response.body()
+                    Log.e("locations", locations.toString())
+                }
+                is Result.Failure -> {
+                    Log.e("locations", result.error.toString())
+                }
+            }
+        }
     }
 }
+
+
