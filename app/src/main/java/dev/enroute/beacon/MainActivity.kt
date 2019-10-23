8b042d4c619433a691d82cf6691884b1f40405f2
eric-tong <erictong.designs@gmail.com>
2019-10-23 17:50:42 +0100

    ENR-4 Send location from device with permissions

--- app/src/main/java/dev/enroute/beacon/MainActivity.kt
+++ app/src/main/java/dev/enroute/beacon/MainActivity.kt
@@ -1,31 +1,54 @@
 package dev.enroute.beacon
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.enroute.beacon.R
 import dev.enroute.beacon.api.PlatformApi
-import dev.enroute.beacon.helper.enqueue
 import dev.enroute.beacon.helper.Result
+import dev.enroute.beacon.helper.enqueue
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LocationListener {
+    private val service: PlatformApi = Retrofit.Builder()
+        .baseUrl("https://enroute-platform.herokuapp.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create<PlatformApi>(PlatformApi::class.java)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val service = Retrofit.Builder()
-            .baseUrl("https://enroute-platform.herokuapp.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create<PlatformApi>(PlatformApi::class.java)
+        val locationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
-        val lat = 444F
-        val long = 444F
+        if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 11
+            )
+        }
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 11
+            )
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0F, this)
+    }
 
-        service.putLocation(lat, long).enqueue { result ->
+    override fun onLocationChanged(location: Location?) {
+        service.putLocation(location!!.latitude, location.longitude).enqueue { result ->
             when (result) {
                 is Result.Success -> {
                     val locations = result.response.body()
@@ -36,6 +59,18 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderEnabled(provider: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderDisabled(provider: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
 
