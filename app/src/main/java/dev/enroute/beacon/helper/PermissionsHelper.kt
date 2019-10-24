8b5996f376012343221e094bce8365050c63c62f
eric-tong <erictong.designs@gmail.com>
2019-10-24 14:35:08 +0100

    ENR-5 Add permissionsHelper

--- app/src/main/java/dev/enroute/beacon/helper/PermissionsHelper.kt
+++ app/src/main/java/dev/enroute/beacon/helper/PermissionsHelper.kt
@@ -5,16 +5,16 @@ import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
 
+val PERMISSIONS = arrayOf(
+    Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+    Manifest.permission.ACCESS_COARSE_LOCATION,
+    Manifest.permission.ACCESS_FINE_LOCATION
+)
+
 fun hasLocationPermissions(context: Context): Boolean {
-    return (context.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) &&
-            (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+    return PERMISSIONS.all { context.checkSelfPermission(it) != PackageManager.PERMISSION_GRANTED }
 }
 
 fun requestLocationPermissions(activity: Activity) {
-    activity.requestPermissions(
-        arrayOf(
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        ), 11
-    )
+    activity.requestPermissions(PERMISSIONS, 11)
 }
